3c05330dedf67a0850515a05ff23cecc5b4e0776
Carlos Herver <carlos.h.s.1994@gmail.com>
2022-03-14 17:24:17 -0600

    Finish header for mobile version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import './style/main.scss';
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import Hamburgerbutton from '../img/hamburger-button.svg';
+import CloseButton from '../img/close-button.svg';
+import Envelope from '../img/envelope.svg';
 
 const Header = () => (
   <header>
     <a href="/">Carlos Herver</a>
     <nav>
       <img src={Hamburgerbutton} alt="Hamburger button" />
+      <ul>
+        <li className="close">
+          <button type="button">
+            <img src={CloseButton} alt="close" />
+          </button>
+        </li>
+        <li><a href="#works-section-id">Portafolio</a></li>
+        <li><a href="#about-section-id">About</a></li>
+        <li><a href="#contact-section-id">Contact</a></li>
+        <li className="envelope">
+          <a href="/">
+            <img src={Envelope} alt="get my e-mail" />
+          </a>
+        </li>
+      </ul>
     </nav>
   </header>
 );
