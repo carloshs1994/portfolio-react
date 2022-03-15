6b16f420eeb322fba9e567c6b50936dbd7814223
Carlos Herver <carlos.h.s.1994@gmail.com>
2022-03-14 18:43:52 -0600

    Finish first section on mobile version

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import './style/main.scss';
+import Presentation from './components/Presentation';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <section className="head">
-        <h1>Hey</h1>
-      </section>
+      <Presentation />
     </div>
   );
 }
