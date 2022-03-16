104910092ab690b76343ebd653fb83a4277097b0
Carlos Herver <carlos.h.s.1994@gmail.com>
2022-03-16 10:41:16 -0600

    Add actions to hamburger and close button for mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,39 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Hamburgerbutton from '../img/hamburger-button.svg';
 import CloseButton from '../img/close-button.svg';
 import Envelope from '../img/envelope.svg';
+import { openMobileMenu, closeMobileMenu } from '../redux/home/home';
 
-const Header = () => (
-  <header>
-    <a href="/">Carlos Herver</a>
-    <nav>
-      <img src={Hamburgerbutton} alt="Hamburger button" />
-      <ul>
-        <li className="close">
-          <button type="button">
-            <img src={CloseButton} alt="close" />
-          </button>
-        </li>
-        <li><a href="#works-section-id">Portafolio</a></li>
-        <li><a href="#about-section-id">About</a></li>
-        <li><a href="#contact-section-id">Contact</a></li>
-        <li className="envelope">
-          <a href="/">
-            <img src={Envelope} alt="get my e-mail" />
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const dispatch = useDispatch();
+  const statusMobileMenu = useSelector((state) => state.homeReducer.displayMobileMenu);
+  const styleMobileMenu = (statusMobileMenu) ? '0' : '-150%';
+  return (
+    <header>
+      <a href="/">Carlos Herver</a>
+      <nav>
+        <button onClick={() => dispatch(openMobileMenu())} type="button">
+          <img src={Hamburgerbutton} alt="Hamburger button" />
+        </button>
+        <ul style={{ left: styleMobileMenu }}>
+          <li className="close">
+            <button onClick={() => dispatch(closeMobileMenu())} type="button">
+              <img src={CloseButton} alt="close" />
+            </button>
+          </li>
+          <li><a href="#works-section-id">Portafolio</a></li>
+          <li><a href="#about-section-id">About</a></li>
+          <li><a href="#contact-section-id">Contact</a></li>
+          <li className="envelope">
+            <a href="/">
+              <img src={Envelope} alt="get my e-mail" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- src/redux/home/home.js
+++ src/redux/home/home.js
@@ -1,5 +1,17 @@
+const OPEN_MOBILE_MENU = 'portfolio-react/homeReducer/OPEN_MOBILE_MENU';
+const CLOSE_MOBILE_MENU = 'portfolio-react/homeReducer/CLOSE_MOBILE_MENU';
 const OPEN_MODAL = 'portfolio-react/homeReducer/OPEN_MODAL';
-const initialState = {};
+const initialState = {
+  displayMobileMenu: false,
+};
+
+export const openMobileMenu = () => ({
+  type: OPEN_MOBILE_MENU,
+});
+
+export const closeMobileMenu = () => ({
+  type: CLOSE_MOBILE_MENU,
+});
 
 export const openModal = (payload) => ({
   type: OPEN_MODAL,
@@ -10,6 +22,10 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case OPEN_MODAL:
       return action.payload;
+    case OPEN_MOBILE_MENU:
+      return { ...state, displayMobileMenu: true };
+    case CLOSE_MOBILE_MENU:
+      return { ...state, displayMobileMenu: false };
     default:
       return state;
   }
